5a4c7d83445b5711c5678013738e29c862fff12f
Colin Clark <colinbdclark@gmail.com>
2025-03-19 17:19:26 -0400

    Outputs arguments received over OSC.

--- src/joint-listener.h
+++ src/joint-listener.h
@@ -10,9 +10,33 @@ class JointListener: public osc::OscPacketListener  {
         const IpEndpointName& remoteEndpoint ) {
             (void) remoteEndpoint;
         try {
-            std::cout << "received message with address " << m.AddressPattern()
-                << std::endl;
+            osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+            float x1, y1;
+            float x2, y2;
+            float x3, y3;
+            float x4, y4;
+            float x5, y5;
+            float x6, y6;
+            float x7, y7;
 
+            args >> x1 >> y1
+                >> x2 >> y2
+                >> x3 >> y3
+                >> x4 >> y4
+                >> x5 >> y5
+                >> x6 >> y6
+                >> x7 >> y7
+                >> osc::EndMessage;
+
+            std::cout << m.AddressPattern()
+                << " [" << x1 << ", " << y1 << "],"
+                << "[" << x2 << ", " << y2 << "],"
+                << "[" << x3 << ", " << y3 << "],"
+                << "[" << x4 << ", " << y4 << "],"
+                << "[" << x5 << ", " << y5 << "],"
+                << "[" << x6 << ", " << y6 << "],"
+                << "[" << x7 << ", " << y7 << "]"
+                << std::endl;
         } catch(osc::Exception& e) {
             std::cout << "error while parsing message: "
                 << m.AddressPattern() << ": " << e.what() << "\n";
